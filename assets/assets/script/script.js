b83b55a08a73270cbab0300e71898f664a6e3c58
Aidan Chamberlain <asunshine99@gmail.com>
2022-02-02 21:48:12 -0600

    bootstrap styling, comments

--- assets/assets/script/script.js
+++ assets/assets/script/script.js
@@ -2,6 +2,7 @@
 const searchCities = [];
 
 // functions
+// finds the latitude and longitude of input
 function handleCoords(searchCity) {
     const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=4b9f7dc3f8536150bc0eb915e8e4a81b`;
 
@@ -14,14 +15,17 @@ function handleCoords(searchCity) {
             }
         })
         .then(function (data) {
+            // handles current weather at given lat and lon
             handleCurrentWeather(data.coord, data.name);
         })
         .catch((error) => {
-            console.log("Error");
+            console.log(error);
         });
 }
 
+// populates current weather and 5 day forecast for a given city with coordinates
 function handleCurrentWeather(coordinates, city) {
+    // assigns vars to latitude and longitude
     const lat = coordinates.lat;
     const lon = coordinates.lon;
 
@@ -32,17 +36,22 @@ function handleCurrentWeather(coordinates, city) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            // populates current weather area
             displayCurrentWeather(data.current, city);
+            // populates 5 day forecast
             displayFiveDayWeather(data.daily);
         });
 }
 
+// displays information in current weather area
 function displayCurrentWeather(currentCityData, cityName) {
+    // icon representing weather conditions
     let weatherIcon = `http://openweathermap.org/img/wn/${currentCityData.weather[0].icon}.png`;
-    // todo: add Wind, humidity, UV index DONT FORGET UNITS
-    // create dynamic bg for uv index by adding class based on value of uv
-    document.querySelector("#currentWeather").innerHTML = `<h2>${cityName}, ${moment.unix(currentCityData.dt).format("MMM Do YY")} <img src="${weatherIcon}"></h2> <div>Temp: ${currentCityData.temp} \xB0F</div> <div>Wind Speed: ${currentCityData.wind_speed} MPH</div> <div>Humidity: ${currentCityData.humidity}%</div> <div id="uvIndex">UV Index: ${currentCityData.uvi}</div>`;
+    // adds "card" styling from Bootstrap, is not added in index.html because initializing with the card style causes an annoying background empty space
+    document.querySelector("#currentWeather").classList.add("card");
+    // adds rows of information and displays values
+    document.querySelector("#currentWeather").innerHTML = `<h2 class="h2">${cityName}, ${moment.unix(currentCityData.dt).format("MMM Do YY")} <img src="${weatherIcon}"></h2> <div>Temp: ${currentCityData.temp} \xB0F</div> <div>Wind Speed: ${currentCityData.wind_speed} MPH</div> <div>Humidity: ${currentCityData.humidity}%</div> <div>UV Index: <span  id="uvIndex" class="px-2">${currentCityData.uvi}</span></div>`;
+    // checks how severe the UV Index is, and changes background color of data to indicate severity
     if (currentCityData.uvi >= 0 && currentCityData.uvi < 3) {
         document.querySelector("#uvIndex").classList.add("uvLow");
     } else if (currentCityData.uvi >= 3 && currentCityData.uvi < 6) {
@@ -56,42 +65,56 @@ function displayCurrentWeather(currentCityData, cityName) {
     }
 }
 
+// displays information in the 5 day forecast area
 function displayFiveDayWeather(fiveDayCityData) {
+    // removes data points that are not the next 5 following days
     const cityData = fiveDayCityData.slice(1, 6);
+    // initializes 5 day forecast display area
     document.querySelector("#fiveDayWeather").innerHTML = "";
 
+    // creates a card for each day and displays the relevant information inside the card
     cityData.forEach((day) => {
+        // icon representing weather conditions
         let weatherIcon = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
-        // todo: temp, wind, humidity DONT FORGET UNITS ()
-        document.querySelector("#fiveDayWeather").innerHTML += `<div><div>${moment.unix(day.dt).format("MMM Do YY")}</div> <div><img src="${weatherIcon}"></div> <div>Temp: ${day.temp.day} \xB0F</div> <div>Wind Speed: ${day.wind_speed} MPH</div> <div>Humidity: ${day.humidity}%</div></div>`;
+        // adds rows of information and displays values
+        document.querySelector("#fiveDayWeather").innerHTML += `<div class="bg-dark card m-1 p-2 text-white"><div class="h3">${moment.unix(day.dt).format("MMM Do YY")}</div> <div><img src="${weatherIcon}"></div> <div>Temp: ${day.temp.day} \xB0F</div> <div>Wind Speed: ${day.wind_speed} MPH</div> <div>Humidity: ${day.humidity}%</div></div>`;
     });
 }
 
+// accepts user input from form, populates search history, kicks off weather data retrieval
 function handleFormSubmit(event) {
+    // initializes history buttons
     document.querySelector("#searchHistory").innerHTML = "";
     event.preventDefault();
 
+    // assigns var to input data
     const city = document.querySelector("#searchInput").value.trim();
+    // adds input data to history array
     searchCities.push(city.toLowerCase());
 
+    // filters out repeat city names in the history array
     const filteredCities = searchCities.filter((city, index) => {
         return searchCities.indexOf(city) === index;
     });
 
+    // creates clickable button for each city in the history array
     filteredCities.forEach((city) => {
-        document.querySelector("#searchHistory").innerHTML += `<button data-city="${city}">${city}</button>`;
+        document.querySelector("#searchHistory").innerHTML += `<button class="btn btn-secondary d-block m-2" data-city="${city}">${city}</button>`;
     });
+    // finds the latitude and longitude of input
     handleCoords(city);
 }
 
+// runs data retrieval if user clicks on a search history button
 function handleHistory(event) {
+    // assigns var to city name, pulled from data-attribute on the search history button
     const city = event.target.getAttribute("data-city");
+    // finds the latitude and longitude of input
     handleCoords(city);
 }
 
 // listeners
-// on page load, show any past cities searched
-// search for city
-// click on city to show weather
+// data retrieval when user uses the form
 document.querySelector("#searchForm").addEventListener("submit", handleFormSubmit);
+// data retrieval when user uses the search history
 document.querySelector("#searchHistory").addEventListener("click", handleHistory);
